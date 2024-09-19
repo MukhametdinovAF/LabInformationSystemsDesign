77676656aa200dc7554f347289be222d3ace547b
Артур Мухаметдинов <artur@MacBook-Pro-Artur.local>
2024-09-19 09:13:47 +0500

    Initial commit

---
+++ src/lab5/Main.java
@@ -0,0 +1,25 @@
+package lab5;
+import additionalFunctions.DocumentReader;
+import additionalFunctions.SplitLines;
+
+import java.util.ArrayList;
+
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList <String> list  = DocumentReader.FileLineReader("src/lab5/file1"); // читаем данные из файла и сохраняем в массив
+        double [] averageRecoveryTime = SplitLines.splitLines(SplitLines.subStrings(list,2)); // массив с пороговыми значениями для параметра Тв, взятый из прочитанного файла
+        double [] estDurTransIO = SplitLines.splitLines(SplitLines.subStrings(list,4)); // массив с пороговыми значениями для параметра Тп, взятый из прочитанного файла
+        double probability = ProbabilityWithoutFailures.probabilityCalculations(list); // рассчет P
+        double averageRecTimeEstimate = AverageRecoveryTime.averageRecoveryTimeEstimate(list,AverageRecoveryTime.recoveryTime(averageRecoveryTime)); // рассчет Qв
+        double estDurTransformIO = EstimationDurationTransformationIO.estimationDurationIO(list,EstimationDurationTransformationIO.estimateDuration(EstimationDurationTransformationIO.actualConversionDuration(estDurTransIO))); // рассчет Qп
+        System.out.println("Вероятность безотказной работы (P): "+probability);
+        System.out.println("Оценка по среднему времени восстановления (Qв): "+averageRecTimeEstimate);
+        System.out.println("Оценка по продолжительности преобразования входного набора данных в выходной (Qп): "+estDurTransformIO);
+        System.out.println("Итоговая оценка вероятности безотказной работы : "+probability);
+        System.out.println("Итоговая оценка по среднему времени восстановления и продолжительности преобразования : "+ MetricsCalculations.finalGradeCalculation(averageRecTimeEstimate,estDurTransformIO));
+        System.out.println("Абсолютный показатель критериев : "+ MetricsCalculations.absoluteIndicatorsCalculation(probability, MetricsCalculations.finalGradeCalculation(averageRecTimeEstimate,estDurTransformIO)));
+        System.out.println("Относительный показатель критериев : "+ MetricsCalculations.relativeIndicatorsCalculation(MetricsCalculations.absoluteIndicatorsCalculation(probability, MetricsCalculations.finalGradeCalculation(averageRecTimeEstimate,estDurTransformIO)),Double.parseDouble(list.get(6))));
+        System.out.println("Фактор надежности : "+ MetricsCalculations.relativeIndicatorsCalculation(MetricsCalculations.absoluteIndicatorsCalculation(probability, MetricsCalculations.finalGradeCalculation(averageRecTimeEstimate,estDurTransformIO)),Double.parseDouble(list.get(6))));
+    }
+}
---
+++ src/lab5/ProbabilityWithoutFailures.java
@@ -0,0 +1,11 @@
+package lab5;
+
+import java.util.ArrayList;
+// Оценка вероятности безотказной работы P
+public class ProbabilityWithoutFailures {
+    public static double probabilityCalculations (ArrayList <String> lines) {
+        int numberOfExperiments = Integer.parseInt(lines.get(1)); // Количество экспериментов N
+        int numberOfFailures = Integer.parseInt(lines.get(0)); // Количество отказов Q
+        return 1 - (double) numberOfFailures / (double) numberOfExperiments; // Возврат вероятности безотказной работы
+    }
+}
